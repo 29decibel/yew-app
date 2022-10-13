96ce308fc738b09d5861e9a5b1a9aacabd1779c1
29decibel <m@idots.me>
2022-10-13 08:42:59 -0700

    parse posts

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,59 @@ impl Component for Model {
     }
 }
 
+use glob::glob_with;
+use glob::MatchOptions;
+
+#[derive(Debug)]
+struct Post {
+    relative_path: String,
+    tags: Vec<String>,
+    title: String,
+    description: String,
+    draft: bool,
+    content: String,
+}
+
+fn get_posts(dir: &str) -> std::vec::Vec<std::path::PathBuf> {
+    let options = MatchOptions {
+        case_sensitive: false,
+        require_literal_separator: false,
+        require_literal_leading_dot: false,
+    };
+    let markdown_glob = "content/posts/*.md";
+    glob_with(format!("{}/{}", dir, markdown_glob).as_str(), options)
+        .unwrap()
+        .filter_map(|p| p.ok())
+        .collect::<Vec<_>>()
+}
+
+fn get_post_from_path(path: &std::path::Path) -> Post {
+    // reading the path and parse
+    Post {
+        relative_path: String::from(path.to_str().unwrap()),
+        tags: vec![String::from("tag1"), String::from("tag2")],
+        title: String::from("some title"),
+        description: String::from("some description"),
+        draft: false,
+        content: String::from(""),
+    }
+}
+
+fn convert_to_posts(dir: &str, post_paths: std::vec::Vec<std::path::PathBuf>) -> Vec<Post> {
+    let nice: Vec<Post> = post_paths
+        .iter()
+        .filter_map(|pb| pb.strip_prefix(dir).ok())
+        .map(|p| get_post_from_path(p))
+        .collect();
+
+    nice
+}
+
 fn main() {
-    yew::start_app::<Model>();
+    let dir = "/Users/dongbinli/sites/orchardlabdev-site";
+    let results = get_posts(dir);
+    let posts = convert_to_posts(dir, results);
+    // parse all the results and convert them into Post array
+    println!("{:?}", posts);
+    //yew::start_app::<Model>();
 }
